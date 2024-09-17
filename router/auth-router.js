f9430d18a923b93984aec0fd06d6564323c89707
shakibkumnale <shakibkumnali@gmail.com>
2024-09-18 04:12:51 +0530

    everything ok changed the routes

--- router/auth-router.js
+++ router/auth-router.js
@@ -5,15 +5,15 @@ const auth = require('../middleware/auth');
 const {home,register,score,fetchtopic,getquetion, selecttopic,login,otp,user,}=require('../controllers/auth-controller')
 
 
-router.route('/api/users/login').post(login)
-router.route('/otp').post(otp)
-router.route('/').get(home)
-router.route('/fetchtopic').post(auth ,fetchtopic)
-router.route('/f').post( fetchtopic)
+router.route('/api/login').post(login)
+router.route('/api/otp').post(otp)
+router.route('/api/').get(home)
+router.route('/api/fetchtopic').post(auth ,fetchtopic)
+router.route('/api/f').post( fetchtopic)
 router.route('/api/questions/:topic').post(auth,getquetion)
-router.route('/user').get(auth,user)
-router.route('/score').post(auth,score)
-router.route('/select').post(auth,selecttopic)
+router.route('/api/user').get(auth,user)
+router.route('/api/score').post(auth,score)
+router.route('/api/select').post(auth,selecttopic)
 
 router.route('/api/users/register').post(register)
 
